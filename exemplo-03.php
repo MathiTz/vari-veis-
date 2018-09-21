3560650d23c2432ead92d2ee9fcfa7c182a6698c
MathiTz <matheusalves789@gmail.com>
2018-09-21 02:35:31 -0300

    todos os tipos de variáveis

---
+++ exemplo-03.php
@@ -0,0 +1,35 @@
+<?php
+//                  Tipos Básicos 
+//String
+$nome = "Hcode";
+$site = 'www.hcode.com.br';
+//Inteiro
+$ano = 1995;
+//Float
+$salario =  5500.99;
+//Boolean
+$bloqueado = false;
+
+
+//                  Tipos Compostos 
+
+$frutas = array("abacaxi", "laranja", "manga");
+
+//echo $frutas[2];
+
+$nascimento = new DateTime();
+
+//var_dump($nascimento);
+
+//                  Tipos Especiais
+
+$arquivo = fopen("exemplo-03.php", "r");
+
+//var_dump($arquivo);
+
+$nulo = NULL;
+$vazio = "";
+
+
+
+?>
