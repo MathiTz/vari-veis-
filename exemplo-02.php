b24ef25bb19c51c770cdbe9a5cc915a632ad1a2d
MathiTz <matheusalves789@gmail.com>
2018-09-21 02:18:41 -0300

    adicionado variáveis e dois métodos

---
+++ exemplo-02.php
@@ -0,0 +1,28 @@
+<?php
+/*
+$anoNascimento = 1990;
+$nomeCompleto = "";
+$_nome = "Matheus";*/
+$nome1 = "João";
+// $1nome = "João" não funciona
+
+$sobrenome = "Rangel";
+
+$nomeCompleto = $nome1 . " " . $sobrenome;
+
+echo $nomeCompleto;
+
+echo "<br />";
+
+//unset($nome1); elimina da memória
+
+/* verifica se a variável não é null if (isset($nome1)){
+    echo $nome1;
+
+}
+
+echo $nome1; */
+// . " " . concatena e da espaço
+
+
+?>
